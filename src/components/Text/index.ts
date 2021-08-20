c9afb098474077146bf65faf687e1285053bd7ef
gustavo-bueno <gustavobcarvalho22@gmail.com>
2021-08-19 21:16:45 -0300

    feat: custom fonts and text component

---
+++ src/components/Text/index.ts
@@ -0,0 +1,49 @@
+import { TextProps } from 'react-native';
+import styled from 'styled-components/native';
+
+import { fontSize, colors } from '../../styles';
+
+interface ICustomText extends TextProps {
+  color?: string;
+  fontWeight?: 'bold' | 'medium';
+}
+
+const getColor = ({ color = 'text' }: ICustomText) => {
+  return colors[color] || color;
+};
+
+const fontFamily = ({ fontWeight }: ICustomText) => {
+  if (fontWeight == 'bold') return 'Poppins_700Bold';
+  if (fontWeight == 'medium') return 'Poppins_500Medium';
+
+  return 'Poppins_400Regular';
+};
+
+export const H0 = styled.Text<ICustomText>`
+  font-size: ${`${fontSize.h0}px`};
+  font-family: ${fontFamily};
+  color: ${getColor};
+`;
+
+export const H1 = styled.Text<ICustomText>`
+  font-size: ${`${fontSize.h1}px`};
+  font-family: ${fontFamily};
+  color: ${getColor};
+`;
+export const H2 = styled.Text<ICustomText>`
+  font-size: ${`${fontSize.h2}px`};
+  font-family: ${fontFamily};
+  color: ${getColor};
+`;
+
+export const H3 = styled.Text<ICustomText>`
+  font-size: ${`${fontSize.h3}px`};
+  font-family: ${fontFamily};
+  color: ${getColor};
+`;
+
+export const H4 = styled.Text<ICustomText>`
+  font-size: ${`${fontSize.h4}px`};
+  font-family: ${fontFamily};
+  color: ${getColor};
+`;
---
+++ src/styles/index.ts
@@ -0,0 +1,5 @@
+import colors from './variables/colors';
+import fontSize from './variables/fontSize';
+import metrics from './variables/metrics';
+
+export { colors, fontSize, metrics };
